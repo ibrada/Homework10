bbbb06b3d87b7cbc098590d8caa9779191c21a8b
ibrada <radikibragimov@mail.ru>
2023-09-22 00:54:25 +0300

    hw10 is done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
 
 // Task 1
+    public static void task1() {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
+    }
 
 // Task 2
-        fullName = (fullName.toUpperCase());
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName);
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String upFullName = (fullName.toUpperCase());
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + upFullName);
+    }
 
 // Task 3
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные Ф.И.О. сотрудника - " + fullName);
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String repFullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + repFullName);
 
     }
 }
